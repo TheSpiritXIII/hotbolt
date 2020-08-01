3595060f9980f8885a7ee515422d15c09ab6d57e
Daniel Hrabovcak <thespiritxiii@gmail.com>
2020-08-01 14:30:12 -0400

    Move symbol loading to struct

--- hotbolt_runner/src/runner.rs
+++ hotbolt_runner/src/runner.rs
@@ -1,6 +1,6 @@
 use std::{path::Path, sync::mpsc::Receiver};
 
-use libloading as lib;
+use libloading::{Library, Symbol};
 use log::{debug, error, info};
 
 pub enum Event {
@@ -14,56 +14,96 @@ pub enum Event {
 	SetState(Box<[u8]>),
 }
 
+struct HotboltLibSymbols<'a> {
+	run: Symbol<'a, unsafe extern "C" fn() -> ()>,
+}
+
+impl<'a> HotboltLibSymbols<'a> {
+	fn from(lib: &'a Library) -> Result<Self, String> {
+		Ok(Self {
+			run: Self::load_symbol(lib, hotbolt_ffi::ENTRY_MAIN)?,
+		})
+	}
+
+	fn load_symbol<T: 'a>(lib: &'a Library, name: &'static str) -> Result<Symbol<'a, T>, String> {
+		unsafe {
+			let func: Result<Symbol<T>, _> = lib.get(name.as_bytes());
+			func
+		}
+		.map_err(|_err| format!("Error loading symbol `{}`", name))
+	}
+
+	fn run(&self) {
+		unsafe { (self.run)() }
+	}
+}
+
+struct HotboltLib {
+	lib: Library,
+}
+
+impl HotboltLib {
+	fn load<P: AsRef<Path>>(path: P) -> Result<HotboltLib, String> {
+		Library::new(path.as_ref().as_os_str())
+			.map(|lib| Self { lib })
+			.map_err(|_err| "Error loading entry point".to_owned())
+	}
+
+	fn symbols<'a>(&'a self) -> Result<HotboltLibSymbols<'a>, String> {
+		HotboltLibSymbols::from(&self.lib)
+	}
+}
+
 pub fn run<P: AsRef<Path>>(path: P, reciever: Receiver<Event>) {
 	let mut _state: Option<Box<[u8]>> = None;
 	'lib_load: loop {
 		let load_error;
 
-		if let Ok(lib) = lib::Library::new(path.as_ref().as_os_str()) {
-			info!("Successfully loaded library");
-			// TODO: Call init symbol with state.
+		match HotboltLib::load(&path) {
+			Ok(lib) => {
+				info!("Successfully loaded library");
+				
+				match lib.symbols() {
+					Ok(symbols) => {
+						debug!("Successfully loaded entry point");
+						// TODO: Call init symbol with state.
 
-			let entry_point = unsafe {
-				let func: Result<lib::Symbol<unsafe extern "C" fn() -> ()>, _> =
-					lib.get(hotbolt_ffi::ENTRY_MAIN.as_bytes());
-				func
-			};
-			if let Ok(symbol) = entry_point {
-				debug!("Successfully loaded entry point");
-				loop {
-					if let Ok(event) = reciever.try_recv() {
-						match event {
-							Event::Start => {
-								// Ignore this.
+						loop {
+							if let Ok(event) = reciever.try_recv() {
+								match event {
+									Event::Start => {
+										// Ignore this.
+									}
+									Event::Restart => {
+										info!("Restarting application");
+										_state = None;
+										continue 'lib_load;
+									}
+									Event::Reload => {
+										info!("Reloading library");
+										// TODO: Update state.
+										_state = None;
+										continue 'lib_load;
+									}
+									Event::SetState(update) => {
+										info!("Updating state");
+										_state = Some(update);
+									}
+								}
 							}
-							Event::Restart => {
-								info!("Restarting application");
-								_state = None;
-								continue 'lib_load;
-							}
-							Event::Reload => {
-								info!("Reloading library");
-								// TODO: Update state.
-								_state = None;
-								continue 'lib_load;
-							}
-							Event::SetState(update) => {
-								info!("Updating state");
-								_state = Some(update);
-							}
+							symbols.run();
 						}
 					}
-					unsafe {
-						symbol();
+					Err(err) => {
+						error!("{}", err);
+						load_error = true;
 					}
 				}
-			} else {
-				error!("Error loading entry point");
+			}
+			Err(err) => {
+				error!("{}", err);
 				load_error = true;
 			}
-		} else {
-			error!("Unable to load library");
-			load_error = true;
 		}
 
 		// Must now loop until we get confirmation to restart.
