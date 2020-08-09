f5a75d48d0ddc9cbea24d11aae44956aff53a6cf
Daniel Hrabovcak <thespiritxiii@gmail.com>
2020-08-08 21:44:56 -0400

    Add new entry macros

--- hotbolt_ffi/src/lib.rs
+++ hotbolt_ffi/src/lib.rs
@@ -1,8 +1,13 @@
+use std::io::{Read, Write};
 use std::{ffi::c_void, os::raw::c_char};
 
 pub static ENTRY_VERSION: &str = "hotbolt_entry_version";
+pub static ENTRY_IS_COMPATIBLE: &str = "hotbolt_entry_is_compatible";
+
 pub static ENTRY_INIT: &str = "hotbolt_entry_init";
 pub static ENTRY_MAIN: &str = "hotbolt_entry_main";
+pub static ENTRY_DESTROY: &str = "hotbolt_entry_destroy";
+
 pub static ENTRY_STATE: &str = "hotbolt_entry_state";
 
 #[repr(C)]
@@ -73,7 +78,10 @@ impl FfiServer {
 			server.reload();
 		}
 
-		unsafe extern "C" fn server_ffi_reload_with<T: Server>(arg: *const c_void, state: SizedCharArray) {
+		unsafe extern "C" fn server_ffi_reload_with<T: Server>(
+			arg: *const c_void,
+			state: SizedCharArray,
+		) {
 			let pointer: *const T = arg as *const T;
 			let server: &dyn Server = &*pointer;
 			server.reload_with(state.as_u8_slice());
@@ -97,5 +105,93 @@ impl Server for FfiServer {
 	}
 	fn reload_with(&self, state: &[u8]) {
 		unsafe { (self.reload_with)(self.server, SizedCharArray::from_slice(&state)) }
+	}
+}
+
+/// Serializes and deserializes the application state.
+pub trait Serializer<T> {
+	// Writes the given value into the writer.
+	fn serialize<W: Write>(writer: &W, value: T) -> Result<(), ()>;
+
+	// Reads the given value as T from the reader.
+	fn deserialize<R: Read>(reader: R) -> Result<T, ()>;
+}
+
+pub trait Compatibility {
+	// Returns a static string indicating the version number of the application.
+	fn version() -> &'static str;
+
+	// Returns true if the given other version is compatible with this one.
+	fn is_compatible(other: &str) -> bool {
+		Self::version() == other
+	}
+}
+
+/// Represents the application and its state.
+pub trait Client {
+	type T;
+	type Serializer: Serializer<Self::T>;
+	type Compatibility: Compatibility;
+
+	/// Creates the client. Only called on initialization or if the client is incompatible with the last run client.
+	fn create() -> Self;
+
+	/// The main entry point for the application.
+	fn run(&mut self, server: impl Server, state: Option<Self::T>);
+
+	/// Returns the current application state.
+	fn destroy(self);
+
+	/// Returns the current application state.
+	fn state(&self) -> Option<Self::T>;
+}
+
+/// A low level version of `Client`.
+pub trait FfiClient {
+	fn run(&mut self, server: FfiServer, state: SizedCharArray);
+	fn destroy(self);
+	fn state(&self) -> SizedCharArray;
+	fn version() -> *const u8;
+	fn is_compatible(other: *const u8) -> bool;
+}
+
+impl<T: Client> FfiClient for T {
+	fn run(&mut self, server: FfiServer, state: SizedCharArray) {
+		let opt = if !state.is_empty() {
+			T::Serializer::deserialize(state.as_u8_slice()).ok()
+		} else {
+			None
+		};
+		Client::run(self, server, opt);
+	}
+
+	fn destroy(self) {
+		self.destroy();
+	}
+
+	fn state(&self) -> SizedCharArray {
+		if let Some(state) = Client::state(self) {
+			let v: Vec<u8> = Vec::new();
+			if T::Serializer::serialize(&v, state).is_ok() {
+				let char_array = SizedCharArray {
+					array: v.as_ptr() as *const c_char,
+					len: v.len(),
+				};
+				std::mem::forget(v);
+				char_array
+			} else {
+				SizedCharArray::empty()
+			}
+		} else {
+			SizedCharArray::empty()
+		}
+	}
+
+	fn version() -> *const u8 {
+		T::Compatibility::version().as_ptr()
+	}
+
+	fn is_compatible(_other: *const u8) -> bool {
+		todo!()
 	}
 }
--- hotbolt_macro/src/lib.rs
+++ hotbolt_macro/src/lib.rs
@@ -1,30 +1,169 @@
-use hotbolt_ffi::{ENTRY_STATE, ENTRY_MAIN};
+use hotbolt_ffi::{
+	ENTRY_DESTROY, ENTRY_INIT, ENTRY_IS_COMPATIBLE, ENTRY_MAIN, ENTRY_STATE, ENTRY_VERSION,
+};
 use proc_macro::TokenStream;
 use quote::{format_ident, quote};
-use syn::{Ident, Item};
-
+use syn::{Ident, Item, Signature};
 
 #[proc_macro_attribute]
-pub fn hotbolt_entry_state(_attr: TokenStream, token_stream: TokenStream) -> TokenStream {
+pub fn hotbolt_main(_attr: TokenStream, token_stream: TokenStream) -> TokenStream {
 	let input: Item = syn::parse_macro_input!(token_stream);
-	if let Item::Fn(input_function) = &input {
-		let input_function_name = &input_function.sig.ident;
-		let entry_name = format_ident!("{}", ENTRY_STATE);
+	let client_type = match &input {
+		Item::Struct(item) => item,
+		_ => panic!("#[{}] is intended on a struct", "hotbolt_main"),
+	};
 
-		let expanded = quote! {
-			#input
+	let client_name = &client_type.ident;
 
-			#[cfg(not(feature = "hotbolt_erase"))]
-			#[no_mangle]
-			pub extern "C" fn #entry_name() -> hotbolt::internal::SizedCharArray {
-				hotbolt::internal::SizedCharArray::from_slice(&#input_function_name())
+	let expanded = quote! {
+		type HotboltEntryClient: hotbolt::Client = #client_name;
+
+		#[hotbolt::hotbolt_entry_state]
+		fn hotbolt_auto_version() {
+			todo!();
+		}
+
+		#[hotbolt::hotbolt_entry_is_compatible]
+		fn hotbolt_auto_is_compatible() {
+			todo!();
+		}
+
+		#[hotbolt::hotbolt_entry_init]
+		fn hotbolt_auto_init() {
+			todo!();
+		}
+
+		#[hotbolt::hotbolt_entry_main]
+		fn hotbolt_auto_main(server: impl Server, state: &[u8]) {
+			todo!();
+		}
+
+		#[hotbolt::hotbolt_entry_destroy]
+		fn hotbolt_auto_destroy() {
+			todo!();
+		}
+
+		#[hotbolt::hotbolt_entry_state]
+		fn hotbolt_auto_state() -> Vec<u8> {
+			todo!();
+		}
+
+		#[hotbolt::hotbolt_entry_version]
+		fn hotbolt_auto_version() -> Vec<u8> {
+			todo!();
+		}
+	};
+
+	TokenStream::from(expanded)
+}
+
+fn wrap_method<TSignature, TBody>(
+	name: &str,
+	token_stream: TokenStream,
+	signature_fn: TSignature,
+	body_fn: TBody,
+) -> TokenStream
+where
+	TSignature: Fn() -> proc_macro2::TokenStream,
+	TBody: Fn(&Signature) -> proc_macro2::TokenStream,
+{
+	let input: Item = syn::parse_macro_input!(token_stream);
+	let input_function = match &input {
+		Item::Fn(item) => item,
+		_ => panic!("#[{}] is intended on a function", name),
+	};
+
+	let sig = signature_fn();
+	let body = body_fn(&input_function.sig);
+	let expanded = quote! {
+		#input
+
+		#[cfg(not(feature = "hotbolt_erase"))]
+		#[no_mangle]
+		pub extern "C" #sig {
+			#body
+		}
+	};
+
+	TokenStream::from(expanded)
+}
+
+#[proc_macro_attribute]
+pub fn hotbolt_entry_version(_attr: TokenStream, token_stream: TokenStream) -> TokenStream {
+	wrap_method(
+		"hotbolt_entry_version",
+		token_stream,
+		|| {
+			let ident = format_ident!("{}", ENTRY_VERSION);
+			quote! {
+				fn #ident()
 			}
-		};
+		},
+		|function_ident| {
+			let ident = &function_ident.ident;
+			quote! {
+				#ident()
+			}
+		},
+	)
+}
 
-		TokenStream::from(expanded)
-	} else {
-		panic!("#[hotbolt_entry_main] is intended on a function");
-	}
+#[proc_macro_attribute]
+pub fn hotbolt_entry_is_compatible(_attr: TokenStream, token_stream: TokenStream) -> TokenStream {
+	wrap_method(
+		"hotbolt_entry_is_compatible",
+		token_stream,
+		|| {
+			let ident = format_ident!("{}", ENTRY_IS_COMPATIBLE);
+			quote! {
+				fn #ident()
+			}
+		},
+		|function_ident| {
+			let ident = &function_ident.ident;
+			quote! {
+				#ident()
+			}
+		},
+	)
+}
+
+#[proc_macro_attribute]
+pub fn hotbolt_entry_init(_attr: TokenStream, token_stream: TokenStream) -> TokenStream {
+	wrap_method(
+		"hotbolt_entry_init",
+		token_stream,
+		|| {
+			quote! {
+				fn #ENTRY_INIT()
+			}
+		},
+		|function_ident| {
+			let ident = &function_ident.ident;
+			quote! {
+				#ident()
+			}
+		},
+	)
+}
+
+#[proc_macro_attribute]
+pub fn hotbolt_entry_destroy(_attr: TokenStream, token_stream: TokenStream) -> TokenStream {
+	wrap_method(
+		"hotbolt_entry_destroy",
+		token_stream,
+		|| {
+			quote! {
+				fn #ENTRY_DESTROY()
+			}
+		},
+		|function_ident| {
+			let ident = &function_ident.ident;
+			quote! {
+				#ident()
+			}
+		},
+	)
 }
 
 // TODO: What to do if the main entry is named main (not allowed to our input)?
@@ -95,4 +234,23 @@ pub fn hotbolt_entry_main(_attr: TokenStream, token_stream: TokenStream) -> Toke
 	} else {
 		panic!("#[hotbolt_entry_main] is intended on a function");
 	}
+}
+
+#[proc_macro_attribute]
+pub fn hotbolt_entry_state(_attr: TokenStream, token_stream: TokenStream) -> TokenStream {
+	wrap_method(
+		"hotbolt_entry_state",
+		token_stream,
+		|| {
+			quote! {
+				fn #ENTRY_STATE() -> hotbolt::internal::SizedCharArray
+			}
+		},
+		|function_ident| {
+			let ident = &function_ident.ident;
+			quote! {
+				hotbolt::internal::SizedCharArray::from_slice(&#ident())
+			}
+		},
+	)
 }
